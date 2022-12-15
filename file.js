4df82e89dbedced6989f52f5ce37e88ad76cf014
akejuwale7@gmail.com <akejuwale7@gmail.com>
2022-12-15 11:59:31 +0100

    A replicate of google made by keju

---
+++ file.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+     $("#btn").click(function(){
+         var a = $("#email").val()
+         var b = $("#pwd").val()
+
+         
+         if(a === "" || b === ""){
+          alert("input field cannot be empty")
+         }
+         
+})
+
+})
+     
